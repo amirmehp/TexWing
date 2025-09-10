e3cbce6211f0b95efae17f55c9a705829d913d8a
amirmehp <mahdipour3070@gmail.com>
2025-09-10 12:59:36 +0330

    Made New File workiiing(UwU)

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,11 @@
 package Main;
 import java.awt.Color;
 import java.awt.event.KeyEvent;
+import java.io.File;
 //import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 import javax.swing.*;
 
@@ -15,27 +17,30 @@ public class Main {
 		 frame.setResizable(true);
 		 frame.setForeground(new Color(0x181818));
 		 frame.setBackground(new Color(0xFFFFFF));
-		 JLabel label = new JLabel("Welcome to TexWing");
-		 label.setHorizontalAlignment(0);
+		 JTextPane editor = new JTextPane();
 		 JMenuBar menuBar = new JMenuBar();
 		 JMenu fileMenu = new JMenu("File");
 		 JMenuItem newItem = new JMenuItem("New File");
 		 newItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
-		 newItem.addActionListener(e -> newFile(label));
+		 newItem.addActionListener(e -> newFile(editor));
 		 fileMenu.add(newItem);
 		 menuBar.add(fileMenu);
 		 frame.setJMenuBar(menuBar);
-		 frame.add(label);
-		 frame.show();
+		 frame.add(editor);
+		 frame.setVisible(true);
 	 }
 
-	 private static Object newFile(JLabel label) {
-		label.setText("New File(^w^)");
+	 private static Object newFile(JTextPane editor) {
 		String path = "./test.txt";
 		try {
 			FileWriter fWriter = new FileWriter(path);
 			fWriter.write("Hello From TexWing:)))");
 			fWriter.close();
+			File myFile = new File(path);
+			Scanner fReader = new Scanner(myFile);
+			editor.setText(fReader.nextLine());
+			fReader.close();
+	
 		} catch (IOException e) {
 			System.out.println("Err: Can't write to file");
 			e.printStackTrace();
